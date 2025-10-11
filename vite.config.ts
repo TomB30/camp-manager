5902c93ca40167bedea9c56a51de0eec4335f994
Tom Bechar <tom@run.ai>
2025-10-11 20:11:33 +0300

    Fix routing: use conditional base path for dev and production

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/camp-manager/',
-})
+  base: mode === 'production' ? '/camp-manager/' : '/',
+}))
 
