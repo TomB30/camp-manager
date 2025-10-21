e025c836f7e281d3200c1aef8fbe6e24a7cbdce6
Tom Bechar <tom@run.ai>
2025-10-21 18:58:51 +0300

    Created base input and button components

---
+++ src/components/common/BaseButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <q-btn
+        v-bind="$attrs"
+        no-caps
+        @click="$emit('click')"
+    >
+        <slot></slot>
+    </q-btn>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "BaseButton",
+    emits: ["click"],
+    methods: {
+        handleClick() {
+            this.$emit("click");
+        },
+    },
+});
+</script>
---
+++ src/components/common/BaseInput.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-input
+    outlined
+    dense
+    :model-value="modelValue"
+    no-error-icon
+    @update:model-value="updateModelValue"
+    v-bind="$attrs"
+  >
+    <template v-slot:prepend>
+      <slot name="prepend"></slot>
+    </template>
+    <template v-slot:append>
+      <slot name="append"></slot>
+    </template>
+  </q-input>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BaseInput",
+  props: {
+    modelValue: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: ["update:modelValue"],
+  methods: {
+    updateModelValue(value: string | number | null) {
+      this.$emit("update:modelValue", value ?? "");
+    },
+  },
+});
+</script>
